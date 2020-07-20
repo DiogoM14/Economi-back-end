fd684f6f0b3894205311e4be32a125e7d0f642a2
Xavi002 <diogomartins200214@gmail.com>
2020-07-20 01:05:42 +0100

    CreateOperations and Operation Models

--- src/database/migrations/1595199734355-CreateOperations.ts
+++ src/database/migrations/1595199734355-CreateOperations.ts
@@ -1,8 +1,57 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateOperations1595199734355
   implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'operations',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user',
+            type: 'varchar',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('operations');
+  }
 }
---
+++ src/repositories/OperationsRepository.ts
@@ -0,0 +1,10 @@
+// import { EntityRepository, Repository, EntityRepository } from 'typeorm';
+
+// import Operation from '../models/Operation'
+
+// @EntityRepository(Operation)
+// class OperationRepository extends Repository<Operation> {
+//   public async getBalance(): Promise<Balance> {
+
+//   }
+// }
