ec7b7d86d4f745b74b9966b4d470f2e3bf8c0ead
Xavi002 <diogomartins200214@gmail.com>
2020-07-27 10:37:58 +0100

    creating user profile

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -6,11 +6,6 @@ import { getCustomRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-import ImportTransactionsService from '../services/ImportTransactionsService';
-
-import uploadConfig from '../config/upload';
-
-const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -46,17 +41,5 @@ transactionsRouter.delete('/:id', async (request, response) => {
 
   return response.status(204).send();
 });
-
-transactionsRouter.post(
-  '/import',
-  upload.single('file'),
-  async (request, response) => {
-    const importTransactions = new ImportTransactionsService();
-
-    const transactions = await importTransactions.execute(request.file.path);
-
-    return response.json(transactions);
-  },
-);
 
 export default transactionsRouter;
